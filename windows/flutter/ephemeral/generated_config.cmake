bf5784b6d019b5e6e1e3b447fa0549057df16c51
shahaman06 <shahaman06@gmail.com>
2024-12-12 18:35:24 -0800

    fixing the branch

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\shaha\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\shaha\\Downloads\\New folder\\measure_converter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\shaha\\flutter"
+  "PROJECT_DIR=C:\\Users\\shaha\\Downloads\\New folder\\measure_converter"
+  "FLUTTER_ROOT=C:\\Users\\shaha\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\shaha\\Downloads\\New folder\\measure_converter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\shaha\\Downloads\\New folder\\measure_converter"
+  "FLUTTER_TARGET=C:\\Users\\shaha\\Downloads\\New folder\\measure_converter\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\shaha\\Downloads\\New folder\\measure_converter\\.dart_tool\\package_config.json"
+)
